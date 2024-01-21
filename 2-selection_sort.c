2077732d35014e8929dc84e5af6c1c18bc86f40d
ADIOZ DANIEL <adiozdaniel@gmail.com>
2024-01-21 05:48:54 +0300

    adding swapper

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -13,3 +13,33 @@ void ints_swapper(int *first_int, int *second_int)
 	*first_int = *second_int;
 	*second_int = temp;
 }
+
+/**
+ * selection_sort - Sort an array of integers in ascending order
+ *                  using the selection sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
+ */
+void selection_sort(int *array, size_t size)
+{
+	int *min;
+	size_t i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array + i;
+		for (j = i + 1; j < size; j++)
+			min = (array[j] < *min) ? (array + j) : min;
+
+		if ((array + i) != min)
+		{
+			ints_swapper(array + i, min);
+			print_array(array, size);
+		}
+	}
+}
